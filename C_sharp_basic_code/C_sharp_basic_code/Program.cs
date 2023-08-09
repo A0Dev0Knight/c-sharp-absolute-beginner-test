74d8853488344ed1595aed1ce3fd1266112d8e00
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-09 13:05:27 +0300

    Functions, variable

--- C_sharp_basic_code/C_sharp_basic_code/Program.cs
+++ C_sharp_basic_code/C_sharp_basic_code/Program.cs
@@ -10,6 +10,23 @@ namespace C_sharp_basic_code
     {
         static void Main(string[] args)
         {
+            int a = 59;
+            Console.WriteLine("Hello world! "+a);
+
+            TestFunction();
+            bool ok = ValueLessThan100(a);
+            Console.WriteLine(ok);
+            
+            Console.ReadKey();
+        }
+
+        static void TestFunction()
+        {
+            Console.WriteLine("This was a secret and you faild to protect it!");
+        }
+        static bool ValueLessThan100(int number)
+        {
+            return number < 100;
         }
     }
 }
