bad86730a1e981f31af32dfdd2d21d1c8c7643c3
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-09 13:29:45 +0300

    foreach, for, Arrays and lists

--- C_sharp_basic_code/C_sharp_basic_code/Program.cs
+++ C_sharp_basic_code/C_sharp_basic_code/Program.cs
@@ -10,12 +10,34 @@ namespace C_sharp_basic_code
     {
         static void Main(string[] args)
         {
+            int[] intArray = new int[5];
+            int[] intArray2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            intList.Add(100);
+            intList.Remove(1);
+
+            foreach(int item in intList)
+            {
+                Console.WriteLine(item);
+            }
+
+            for(int i = 0; i < intList.Count; i++)
+            {
+                Console.WriteLine(i + " " + intList[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static void MiniGame()
+        {
             int age = 20;
 
             if (age < 18)
             {
                 Console.WriteLine("The sindicate will not tolerate this behaviour, report to your supervisor immediately!");
-            } 
+            }
             else Console.WriteLine("You are in the appropiate age range, please proceed!");
 
             switch (age)
@@ -25,15 +47,12 @@ namespace C_sharp_basic_code
                     break;
                 case 17:
                     Console.WriteLine("Soldier you are close to graduating, pacience");
-                        break;
+                    break;
                 case 30:
                     Console.WriteLine("You will be asigned your task for today at station 20 soldier");
-                        break;
+                    break;
             }
-            
-            Console.ReadKey();
         }
-
         static void TestFunction()
         {
             Console.WriteLine("This was a secret and you faild to protect it!");
