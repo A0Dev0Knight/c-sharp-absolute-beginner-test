72096f65e238c8432bbdde17a591e8ba488beacf
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-09 13:16:46 +0300

    Fun minigame

--- C_sharp_basic_code/C_sharp_basic_code/Program.cs
+++ C_sharp_basic_code/C_sharp_basic_code/Program.cs
@@ -10,12 +10,26 @@ namespace C_sharp_basic_code
     {
         static void Main(string[] args)
         {
-            int a = 59;
-            Console.WriteLine("Hello world! "+a);
+            int age = 20;
 
-            TestFunction();
-            bool ok = ValueLessThan100(a);
-            Console.WriteLine(ok);
+            if (age < 18)
+            {
+                Console.WriteLine("The sindicate will not tolerate this behaviour, report to your supervisor immediately!");
+            } 
+            else Console.WriteLine("You are in the appropiate age range, please proceed!");
+
+            switch (age)
+            {
+                default:
+                    Console.WriteLine("Register yourself before enrolling!");
+                    break;
+                case 17:
+                    Console.WriteLine("Soldier you are close to graduating, pacience");
+                        break;
+                case 30:
+                    Console.WriteLine("You will be asigned your task for today at station 20 soldier");
+                        break;
+            }
             
             Console.ReadKey();
         }
